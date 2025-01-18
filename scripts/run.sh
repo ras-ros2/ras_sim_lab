b266c3ebdf0eece809b9817c250f023acb305ac2
Harsh Davda <harsh.davda11@outlook.com>
2025-01-19 05:08:26 +0530

    transport pkg name

--- scripts/run.sh
+++ scripts/run.sh
@@ -19,8 +19,8 @@ tmux send-keys -t main_session:1.0 "ros2 launch rosbridge_server rosbridge_webso
 
 tmux send-keys -t main_session:1.1 "cd /ras_server_app/ras_webapp && npm run dev" C-m
 
-tmux send-keys -t main_session:1.2 "ros2 run ras_aws_transport log_receiver.py" C-m
-tmux send-keys -t main_session:1.3 "ros2 run ras_aws_transport mqtt_broker.py" C-m
+tmux send-keys -t main_session:1.2 "ros2 run ras_transport log_receiver.py" C-m
+tmux send-keys -t main_session:1.3 "ros2 run ras_transport mqtt_broker.py" C-m
 
 tmux new-window -t main_session:2 -n 'experiment'
 tmux split-window -v -t main_session:2
@@ -28,7 +28,7 @@ tmux split-window -v -t main_session:2
 tmux split-window -v -t main_session:2
 
 tmux send-keys -t main_session:2.0 "ros2 run ras_bt_framework experiment_service.py" C-m
-tmux send-keys -t main_session:2.2 "ros2 run ras_aws_transport iot_sender.py" C-m
+tmux send-keys -t main_session:2.2 "ros2 run ras_transport iot_sender.py" C-m
 tmux send-keys -t main_session:2.3 "ros2 run ras_bt_framework FakeGripperServer.py" C-m
 
 
