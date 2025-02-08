d4fd5cdc81f182a9748f8bb33cee1e1cf8640198
Sachin Kumar <sachin.kumar@accelerationrobotics.com>
2025-02-08 11:55:05 +0100

    launch file added for all nodes

--- scripts/run.sh
+++ scripts/run.sh
@@ -19,8 +19,9 @@ tmux send-keys -t main_session:1.0 "ros2 launch rosbridge_server rosbridge_webso
 
 tmux send-keys -t main_session:1.1 "cd /ras_server_app/ras_webapp && npm run dev" C-m
 
-tmux send-keys -t main_session:1.2 "ros2 run ras_transport log_receiver.py" C-m
-tmux send-keys -t main_session:1.3 "ros2 run ras_transport mqtt_broker.py" C-m
+tmux send-keys -t main_session:1.2 "ros2 launch ras_transport server.launch.py" C-m
+# tmux send-keys -t main_session:1.2 "ros2 run ras_transport log_receiver.py" C-m
+# tmux send-keys -t main_session:1.3 "ros2 run ras_transport mqtt_broker.py" C-m
 
 tmux new-window -t main_session:2 -n 'experiment'
 tmux split-window -v -t main_session:2
@@ -28,7 +29,7 @@ tmux split-window -v -t main_session:2
 tmux split-window -v -t main_session:2
 
 tmux send-keys -t main_session:2.0 "ros2 run ras_bt_framework experiment_service.py" C-m
-tmux send-keys -t main_session:2.2 "ros2 run ras_transport iot_sender.py" C-m
+# tmux send-keys -t main_session:2.2 "ros2 run ras_transport iot_sender.py" C-m
 tmux send-keys -t main_session:2.3 "ros2 run ras_bt_framework FakeGripperServer.py" C-m
 
 
@@ -40,6 +41,6 @@ tmux split-window -v -t main_session:3
 
 tmux send-keys -t main_session:3.0 "ros2 run ras_sim spawn_model_node" C-m
 tmux send-keys -t main_session:3.1 "ros2 run ras_sim spawn_manager.py" C-m
-tmux send-keys -t main_session:3.2 "ros2 run ras_transport transport_server_service.py" C-m
+# tmux send-keys -t main_session:3.2 "ros2 run ras_transport transport_server_service.py" C-m
 
 tmux attach-session -t main_session
